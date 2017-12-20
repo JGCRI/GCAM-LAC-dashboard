691edc9c1e2ed29270e9c72e77c805ebf91373fa
Caleb Braun <caleb.braun@pnnl.gov>
2017-12-20 16:59:41 -0500

    Update default data to use IAMRPT output

--- data-raw/gen-default-dataset.R
+++ data-raw/gen-default-dataset.R
@@ -1,5 +1,5 @@
 library(rgcam)
-
+library(iamrpt)
 
 # This function converts GCAM data from wide form to long form
 read.gcam <- function(filename, data.units=NULL, scenario=NULL) {
@@ -27,34 +27,51 @@ read.gcam <- function(filename, data.units=NULL, scenario=NULL) {
   f
 }
 
+# Add gridded water data to the default dataset. The three hydrology files
+# contain half-degree resolution data on water scarcity, water supply, and water
+# demand, which all come from a separate climate and hydrology model.
+addHydroData <- function(defaultData) {
 
-# This dataset includes gridded water data for the landing page. The file
-# hydro.dat is the rgcam compatible file with the clean data.
+  hydro1 <- system.file('extdata/hydrology',
+                        'hydro_scarcity-index_REF_IPSL_2010_2050_2090.csv',
+                        package = "gcamlacdash")
+  hydro2 <- system.file('extdata/hydrology',
+                        'hydro_q_mm-per-mth_rollmean20yr_ipsl-cm5a-lr_hist-rcp8p5_2010_2050_2090.csv',
+                        package = "gcamlacdash")
+  hydro3 <- system.file('extdata/hydrology',
+                        'hydro_demand_mm-per-yr_ipsl_cm5a-lr_hist-rcp8p5_2010_2050_2090.csv',
+                        package = "gcamlacdash")
 
-hydro1 <- system.file('extdata/hydrology',
-                      'hydro_scarcity-index_REF_IPSL_2010_2050_2090.csv',
-                      package = "gcamlacdash")
-hydro2 <- system.file('extdata/hydrology',
-                      'hydro_q_mm-per-mth_rollmean20yr_ipsl-cm5a-lr_hist-rcp8p5_2010_2050_2090.csv',
-                      package = "gcamlacdash")
-hydro3 <- system.file('extdata/hydrology',
-                      'hydro_demand_mm-per-yr_ipsl_cm5a-lr_hist-rcp8p5_2010_2050_2090.csv',
-                      package = "gcamlacdash")
+  scar <- read.gcam(hydro1, data.units="water scarcity index", scenario="Reference")
+  scar <- dplyr::filter(scar, scar$value > 0 & scar$value < 1) # invalid data vals
+  supp <- read.gcam(hydro2, data.units="mm per year", scenario="Reference")
+  dmnd <- read.gcam(hydro3, data.units="mm per year", scenario="Reference")
+  dmnd <- dplyr::filter(dmnd, dmnd$value > 0 & dmnd$value < 50)
 
-scar <- read.gcam(hydro1, data.units="water scarcity index", scenario="Reference")
-scar <- dplyr::filter(scar, scar$value > 0 & scar$value < 1) # invalid data vals
-supp <- read.gcam(hydro2, data.units="mm per year", scenario="REFlu_e6_mex")
-dmnd <- read.gcam(hydro3, data.units="mm per year", scenario="REFlu_e6_mex")
-dmnd <- dplyr::filter(dmnd, dmnd$value > 0 & dmnd$value < 50)
-#Temporary -- while we don't have IAMRPT output as the data
-scar$scenario <- "REFlu_e6_mex"
-supp$scenario <- "REFlu_e6_mex"
-dmnd$scenario <- "REFlu_e6_mex"
+  defaultData <- addQueryTable(defaultData, scar, "Water Scarcity", clobber = T)
+  defaultData <- addQueryTable(defaultData, supp, "Water Supply")
+  defaultData <- addQueryTable(defaultData, dmnd, "Water Demand")
+  defaultData
+}
 
+# This dataset includes queries for a reference scenario and a scenario
+# according to the Paris increased ambitions. The dataset itself is too large to
+# include in the package, but here is how the file was created.
+#
+# conn <- localDBConn('../IDB', 'idb_basexdb')
+# dashboard <- addScenario(conn, 'inst/extdata/dashboard.dat', '../latin_america_dashboard_queries.xml')
+# defaultData <- loadProject(system.file('extdata/dashboard.dat', package = "gcamlacdash"))
+scnctl <- "../dashboard-resources/clean_data/scen.csv"
+varctl <- "../dashboard-resources/clean_data/var.csv"
+dbfldr <- "../IDB"
+generate(scnctl, varctl, dbfldr, fileformat = 'rgcam', wideformat = F)
+if(file.exists('inst/extdata/dashboard.dat')) {
+  warning("File dashboard.dat already exists, copy aborted.")
+} else {
+  file.rename('iamrpt.dat', 'inst/extdata/dashboard.dat')
+}
 defaultData <- loadProject('inst/extdata/dashboard.dat')
-defaultData <- addQueryTable(defaultData, scar, "Water Scarcity", clobber = T)
-defaultData <- addQueryTable(defaultData, supp, "Water Supply")
-defaultData <- addQueryTable(defaultData, dmnd, "Water Demand")
+defaultData <- addHydroData(defaultData)
 
 
 # This dataset includes queries for different SSP scenarios. The dataset itself
@@ -67,14 +84,4 @@ defaultData <- addQueryTable(defaultData, dmnd, "Water Demand")
 sspData <- loadProject(system.file('extdata/ssp.dat', package = "gcamlacdash"))
 
 
-# This dataset includes queries for a reference scenario and a scenario
-# according to the Paris increased ambitions. The dataset itself is too large to
-#  include in the package, but here is how the file was created.
-#
-# conn <- localDBConn('../IDB', 'idb_basexdb')
-# dashboard <- addScenario(conn, 'inst/extdata/dashboard.dat', '../IDB/SSP/ssp_water_latin_america_queries.xml')
-defaultData <- loadProject(system.file('extdata/dashboard.dat', package = "gcamlacdash"))
-
-
 devtools::use_data(sspData, defaultData)
-
